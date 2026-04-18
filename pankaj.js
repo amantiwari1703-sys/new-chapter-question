b8ee5352731e47c852251a3b99bd0115868464b7
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 15:45:44 +0530

    best

--- pankaj.js
+++ pankaj.js
@@ -1,9 +1,8 @@
-// let nums=[30,10,20];
-// nums.sort();
-// console.log(nums);
-let no=[10,20,30];
-no.length=0;
-console.log(no);
+let nums=[10,20,30];
+for(let i=0;i<nums.length;i++){
+    console.log(nums);
+    
+}
 
 
 
