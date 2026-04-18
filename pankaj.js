39f3320b84bc50369f7f79903629c36a380a85ac
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 15:40:07 +0530

    good

--- pankaj.js
+++ pankaj.js
@@ -1,16 +1,9 @@
-// let nums=[10,20,30];
-// console.log(nums.indexOf(30));
-// let no=[10,20,30];
-// let copy =[...no];
-// console.log(copy);
-// let a=[1,2];
-// let b=[3,4];
-// let merged=[...a,...b];
-// console.log(merged);
-let number =[10,20,30];
-number.reverse();
-console.log(number);
-
+// let nums=[30,10,20];
+// nums.sort();
+// console.log(nums);
+let no=[10,20,30];
+no.length=0;
+console.log(no);
 
 
 
