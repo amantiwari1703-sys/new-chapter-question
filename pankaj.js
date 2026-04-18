d46ee3260817de82935bc59f21d50d2c0dcc5972
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 14:23:38 +0530

    length

--- pankaj.js
+++ pankaj.js
@@ -1,8 +1,9 @@
-let nums=[10,30];
-nums.splice(1,0,20);
-console.log(nums);
-let no=[10,20,30];
-no.splice(1,2);
-console.log(no);
+let nums=[10,20,30,40];
+console.log(nums.length);
+console.log(nums.includes(20));
+
+
+
+
 
 
