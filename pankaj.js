5244ddb65aa98cf51049b36e0be056a3850e4d2e
Aman Tiwari <amantiwari@gmail.com>
2026-04-18 14:37:28 +0530

    newarray

--- pankaj.js
+++ pankaj.js
@@ -1,6 +1,19 @@
-let nums=[10,20,30,40];
-console.log(nums.length);
-console.log(nums.includes(20));
+// let nums=[10,20,30];
+// console.log(nums.indexOf(30));
+// let no=[10,20,30];
+// let copy =[...no];
+// console.log(copy);
+// let a=[1,2];
+// let b=[3,4];
+// let merged=[...a,...b];
+// console.log(merged);
+let number =[10,20,30];
+number.reverse();
+console.log(number);
+
+
+
+
 
 
 
